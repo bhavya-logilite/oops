5fa06460889633be8c3544a5a01582e514db6917
bhavya-logilite <87804484+bhavya-logilite@users.noreply.github.com>
2021-08-19 13:44:41 +0530

    practice for thread in java

---
+++ MyThread.java
@@ -0,0 +1,33 @@
+package dlmp;
+class Thread1 extends Thread{
+	@Override
+	public void run() {
+		int i = 0;
+		while(i<=5) {
+			System.out.println("the thread is working for first time"); 
+			i++;
+		}
+	}
+}
+class Thread2 extends Thread{
+	@Override
+	public void run() {
+		int i = 0;
+		while(i<5) {
+			System.out.println("the thread is working for second time"); 
+			i++;
+		}
+	}
+}
+
+public class MyThread {
+
+	public static void main(String[] args) {
+		Thread1 t1 = new Thread1();
+		Thread2 t2 = new Thread2();
+		t1.run();
+		t2.run();
+
+	}
+
+}
