be6179a7bb567c7f845f4883d4e016a191801414
bhavya-logilite <87804484+bhavya-logilite@users.noreply.github.com>
2021-08-19 13:50:24 +0530

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -25,8 +25,8 @@ public class MyThread {
 	public static void main(String[] args) {
 		Thread1 t1 = new Thread1();
 		Thread2 t2 = new Thread2();
-		t1.run();
-		t2.run();
+		t1.start();
+		t2.start();
 
 	}
 
